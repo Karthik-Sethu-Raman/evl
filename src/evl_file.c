00b4521c98ba481e3b15b74dbf45f922b5b217b8
Karthik <karthiksethuraman6@gmail.com>
2026-04-18 11:50:25 +0530

    fix: resolve multiple issues in evl_file.c
    - move NULL check before assert in evl_write_block and evl_read_block
    - remove unused header_ct variable in evl_file_open
    - remove duplicate header_buf declaration in evl_write_block
    - add assert for block_size bounds in evl_write_block and evl_read_block
    - extract evl_seal_header and evl_verify_header to eliminate header auth duplication
    - remove debug printf statements

--- src/evl_file.c
+++ src/evl_file.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <openssl/rand.h>
-
+#include <assert.h>
 #include "evl_file.h"
 #include "evl_types.h"
 #include "kdf.h"
@@ -12,7 +12,56 @@
 #include "nonce.h"
 
 #define HEADER_AAD_STR "EVL_HEADER_V1"
+#define EVL_HEADER_AAD_PREFIX "EVL_HEADER_V1"
+#define EVL_HEADER_AAD_PREFIX_LEN 13
 
+static int evl_seal_header(
+    const uint8_t *header_key,
+    const uint8_t *header_nonce,
+    const uint8_t *header_buf,
+    uint8_t *tag_out
+) {
+    uint8_t aad[EVL_HEADER_AAD_PREFIX_LEN + EVL_HEADER_SIZE];
+
+    memcpy(aad, EVL_HEADER_AAD_PREFIX, EVL_HEADER_AAD_PREFIX_LEN);
+    memcpy(aad + EVL_HEADER_AAD_PREFIX_LEN, header_buf, EVL_HEADER_SIZE);
+
+    uint8_t dummy[1];
+
+    return evl_aes_gcm_encrypt(
+        header_key,
+        header_nonce,
+        aad,
+        sizeof(aad),
+        dummy, 0,
+        dummy,
+        tag_out
+    );
+}
+
+static int evl_verify_header(
+    const uint8_t *header_key,
+    const uint8_t *header_nonce,
+    const uint8_t *header_buf,
+    const uint8_t *tag
+) {
+    uint8_t aad[EVL_HEADER_AAD_PREFIX_LEN + EVL_HEADER_SIZE];
+
+    memcpy(aad, EVL_HEADER_AAD_PREFIX, EVL_HEADER_AAD_PREFIX_LEN);
+    memcpy(aad + EVL_HEADER_AAD_PREFIX_LEN, header_buf, EVL_HEADER_SIZE);
+
+    uint8_t dummy[1];
+
+    return evl_aes_gcm_decrypt(
+        header_key,
+        header_nonce,
+        aad,
+        sizeof(aad),
+        dummy, 0,
+        tag,
+        dummy
+    );
+}
 
 int evl_file_create(
     const char *path,
@@ -37,16 +86,12 @@ int evl_file_create(
 
     uint8_t header_buf[EVL_HEADER_SIZE];
 
-
     fd = open(path, O_CREAT | O_EXCL | O_WRONLY, 0600);
     if (fd < 0)
         goto fail;
 
     if (RAND_bytes(salt, EVL_SALT_SIZE) != 1)
         goto fail;
-    
-    printf("[DBG] create salt: %02X %02X %02X %02X\n",
-    salt[0], salt[1], salt[2], salt[3]);
 
     if (RAND_bytes(file_id, EVL_FILE_ID_SIZE) != 1)
         goto fail;
@@ -54,19 +99,12 @@ int evl_file_create(
     if (RAND_bytes(header_nonce, EVL_HEADER_NONCE_SIZE) != 1)
         goto fail;
     
-    printf("[DBG] create nonce: %02X %02X %02X %02X\n", header_nonce[0], header_nonce[1], header_nonce[2], header_nonce[3]);
-
-
     if (evl_derive_master_key(password, salt, EVL_SALT_SIZE, master_key) != 0)
         goto fail;
 
     if (evl_expand_keys(master_key, EVL_MASTER_KEY_SIZE, enc_key, header_key) != 0)
         goto fail;
     
-    printf("[DBG] create master: %02X %02X %02X %02X\n", master_key[0], master_key[1], master_key[2], master_key[3]);
-    printf("[DBG] create hkey:   %02X %02X %02X %02X\n", header_key[0], header_key[1], header_key[2], header_key[3]);
-
-
     evl_header_t header;
     memset(&header, 0, sizeof(header));
 
@@ -80,19 +118,7 @@ int evl_file_create(
     if (evl_header_serialize(&header, header_buf) != 0)
         goto fail;
 
-
-    uint8_t header_aad[13 + EVL_HEADER_SIZE];
-    memcpy(header_aad, "EVL_HEADER_V1", 13);
-    memcpy(header_aad + 13, header_buf, EVL_HEADER_SIZE);
-    uint8_t dummy[1];
-    if (evl_aes_gcm_encrypt(
-            header_key,
-            header_nonce,
-            header_aad,
-            sizeof(header_aad),
-            dummy, 0, dummy,
-            header_tag
-        ) != 0)
+    if (evl_seal_header(header_key, header_nonce, header_buf, header_tag) != 0)
         goto fail;
 
     if (write(fd, header_buf, EVL_HEADER_SIZE) != EVL_HEADER_SIZE)
@@ -103,7 +129,6 @@ int evl_file_create(
 
     if (write(fd, header_tag, EVL_HEADER_TAG_SIZE) != EVL_HEADER_TAG_SIZE)
         goto fail;
-
 
     out->fd = fd;
     out->header = header;
@@ -157,93 +182,47 @@ int evl_file_open(
     uint8_t header_buf[EVL_HEADER_SIZE];
     uint8_t header_nonce[EVL_HEADER_NONCE_SIZE];
     uint8_t header_tag[EVL_HEADER_TAG_SIZE];
-
-    uint8_t header_ct[EVL_HEADER_SIZE];
-
     uint8_t master_key[EVL_MASTER_KEY_SIZE];
     uint8_t enc_key[EVL_KEY_SIZE];
     uint8_t header_key[EVL_KEY_SIZE];
 
     evl_header_t header;
 
-
     fd = open(path, O_RDWR);
     if (fd < 0)
         goto fail;
 
-
-
     if (read(fd, header_buf, EVL_HEADER_SIZE) != EVL_HEADER_SIZE) {
-        printf("[DBG] failed reading header_buf\n");
         goto fail;
     }
-    printf("[DBG] magic bytes: %02X %02X %02X %02X\n",
-    header_buf[0], header_buf[1], header_buf[2], header_buf[3]);
-
-    printf("[DBG] format_version: %02X\n", header_buf[4]);
-
-    printf("[DBG] block_size bytes: %02X %02X %02X %02X\n",
-    header_buf[61], header_buf[62], header_buf[63], header_buf[64]);
 
     if (read(fd, header_nonce, EVL_HEADER_NONCE_SIZE) != EVL_HEADER_NONCE_SIZE) {
-        printf("[DBG] failed reading header_nonce\n");
         goto fail;
     }
-    printf("[DBG] open nonce:   %02X %02X %02X %02X\n",
-    header_nonce[0], header_nonce[1], header_nonce[2], header_nonce[3]);
 
     if (read(fd, header_tag, EVL_HEADER_TAG_SIZE) != EVL_HEADER_TAG_SIZE) {
-        printf("[DBG] failed reading header_tag\n");
         goto fail;
     }
-    printf("[DBG] open tag:  %02X %02X %02X %02X\n",
-    header_tag[0], header_tag[1], header_tag[2], header_tag[3]);
-
 
     if (evl_header_deserialize(header_buf, &header) != 0) {
-        printf("[DBG] deserialize failed\n");
         goto fail;
     }
-    printf("[DBG] open file_size:  %llu\n", (unsigned long long)header.file_size);
-    printf("[DBG] open salt:   %02X %02X %02X %02X\n", header.salt[0], header.salt[1], header.salt[2], header.salt[3]);
-    printf("[DBG] open file_size:  %llu\n", (unsigned long long)header.file_size);
-
 
     if (evl_derive_master_key(password, header.salt, EVL_SALT_SIZE, master_key) != 0) {
-        printf("[DBG] derive_master_key failed\n");
         goto fail;
     }
 
     if (evl_expand_keys(master_key, EVL_MASTER_KEY_SIZE, enc_key, header_key) != 0) {
-        printf("[DBG] expand_keys failed\n");
         goto fail;
     }
-    printf("[DBG] open master:   %02X %02X %02X %02X\n", master_key[0], master_key[1], master_key[2], master_key[3]);
-    printf("[DBG] open hkey:     %02X %02X %02X %02X\n", header_key[0], header_key[1], header_key[2], header_key[3]);
 
-
-    uint8_t header_aad[13 + EVL_HEADER_SIZE];
-    memcpy(header_aad, "EVL_HEADER_V1", 13);
-    memcpy(header_aad + 13, header_buf, EVL_HEADER_SIZE);
-    uint8_t dummy[1];
-    if (evl_aes_gcm_decrypt(
-            header_key,
-            header_nonce,
-            header_aad,
-            sizeof(header_aad),
-            dummy, 0,
-            header_tag,
-            dummy
-        ) != 0) {
-        printf("[DBG] header tag verification failed\n");
-        goto fail;
-    }
+    if (evl_verify_header(header_key, header_nonce, header_buf, header_tag) != 0)
+    goto fail;
 
     out->fd = fd;
     out->header = header;
     memcpy(out->enc_key, enc_key, EVL_KEY_SIZE);
     memcpy(out->header_key, header_key, EVL_KEY_SIZE);
-    printf("[DBG] deserialize passed\n");
 
     return 0;
 
@@ -258,15 +237,17 @@ fail:
     return -1;
 }
 
+
 int evl_write_block(
     evl_file_t *f,
     uint64_t block_index,
     const uint8_t *data,
     size_t data_len
 ){
+    
     if (!f || !data)
         return -1;
-
+    assert(f->header.block_size <= EVL_DEFAULT_BLOCK_SIZE);
     if (data_len > f->header.block_size)
         return -1;
 
@@ -277,14 +258,9 @@ int evl_write_block(
     uint8_t ciphertext[EVL_DEFAULT_BLOCK_SIZE];
 
     uint8_t aad[EVL_AAD_SIZE];
-    uint8_t header_buf[EVL_HEADER_SIZE];
-
-    
-    
 
     size_t aad_len = 0;
     off_t offset;
-
 
     memset(plaintext_buf, 0, f->header.block_size);
     memcpy(plaintext_buf, data, data_len);
@@ -298,12 +274,8 @@ int evl_write_block(
         &aad_len
     );
 
-
     if (evl_generate_block_nonce(nonce) != 0)
         return -1;
-
-
-    
 
     if (evl_aes_gcm_encrypt(
             f->enc_key,
@@ -317,7 +289,6 @@ int evl_write_block(
         ) != 0)
         return -1;
 
-
     offset =
         EVL_HEADER_SIZE +
         EVL_HEADER_NONCE_SIZE +
@@ -326,7 +297,6 @@ int evl_write_block(
 
     if (lseek(f->fd, offset, SEEK_SET) < 0)
         return -1;
-
 
     if (write(f->fd, nonce, EVL_NONCE_SIZE) != EVL_NONCE_SIZE)
         return -1;
@@ -351,28 +321,12 @@ int evl_write_block(
         if (evl_header_serialize(&f->header, header_buf) != 0)
             goto header_fail;
         
-        printf("[DBG] write file_size: %llu\n", (unsigned long long)f->header.file_size);
-
         if (RAND_bytes(header_nonce, EVL_HEADER_NONCE_SIZE) != 1)
             goto header_fail;
         
-        printf("[DBG] write nonce:  %02X %02X %02X %02X\n", header_nonce[0], header_nonce[1], header_nonce[2], header_nonce[3]);
-
-        uint8_t header_aad[13 + EVL_HEADER_SIZE];
-        memcpy(header_aad, "EVL_HEADER_V1", 13);
-        memcpy(header_aad + 13, header_buf, EVL_HEADER_SIZE);
-        uint8_t dummy[1];
-        if (evl_aes_gcm_encrypt(
-                f->header_key,
-                header_nonce,
-                header_aad,
-                sizeof(header_aad),
-                dummy, 0, dummy,
-                header_tag
-            ) != 0)
+        if (evl_seal_header(f->header_key, header_nonce, header_buf, header_tag) != 0)
             goto header_fail;
-        printf("[DBG] write file_size: %llu\n", (unsigned long long)f->header.file_size); printf("[DBG] write tag: %02X %02X %02X %02X\n", header_tag[0], header_tag[1], header_tag[2], header_tag[3]);
-
+       
         if (lseek(f->fd, 0, SEEK_SET) < 0)
             goto header_fail;
 
@@ -407,9 +361,10 @@ int evl_read_block(
     uint8_t *out,
     size_t *out_len
 ){
+    
     if (!f || !out || !out_len)
         return -1;
-
+    assert(f->header.block_size <= EVL_DEFAULT_BLOCK_SIZE);
     uint32_t block_size = f->header.block_size;
 
 
@@ -439,10 +394,8 @@ int evl_read_block(
         EVL_HEADER_TAG_SIZE +
         block_index * (EVL_NONCE_SIZE + block_size + EVL_TAG_SIZE);
 
-
     if (lseek(f->fd, offset, SEEK_SET) < 0)
         return -1;
-
 
     if (read(f->fd, nonce, EVL_NONCE_SIZE) != EVL_NONCE_SIZE)
         return -1;
@@ -453,7 +406,6 @@ int evl_read_block(
     if (read(f->fd, tag, EVL_TAG_SIZE) != EVL_TAG_SIZE)
         return -1;
 
-
     evl_build_block_aad(
         f->header.file_id,
         block_index,
@@ -461,7 +413,6 @@ int evl_read_block(
         aad,
         &aad_len
     );
-
 
     if (evl_aes_gcm_decrypt(
             f->enc_key,
